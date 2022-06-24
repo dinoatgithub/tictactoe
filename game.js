91e17ddde314f7f7196b5fbf9ca22897d6c14f28
dinoatgithub <dinesh.mishra@att.net>
2022-06-25 02:03:32 +0530

    Phase 1 complete

--- game.js
+++ game.js
@@ -15,6 +15,7 @@ const clickMe = function (id) {
         }
         checkStatus();
     }
+
 }
 
 const startGame = function () {
@@ -27,6 +28,7 @@ const startGame = function () {
     } else {
         document.getElementById("btn").textContent = "Start"
         gameOn = false;
+        textContent = 'X'
         document.getElementById("gameboard").style.display = 'none'
         cleartiles();
     }
@@ -38,14 +40,23 @@ const gameLoaded = function () {
 
 const cleartiles = function () {
     document.getElementById("tl").textContent = '';
+    document.getElementById("tl").setAttribute("class", "topleft");
     document.getElementById("tc").textContent = '';
+    document.getElementById("tc").setAttribute("class", "topcenter");
     document.getElementById("tr").textContent = '';
+    document.getElementById("tr").setAttribute("class", "topright");
     document.getElementById("ml").textContent = '';
+    document.getElementById("ml").setAttribute("class", "middleleft");
     document.getElementById("mc").textContent = '';
+    document.getElementById("mc").setAttribute("class", "middlecenter");
     document.getElementById("mr").textContent = '';
+    document.getElementById("mr").setAttribute("class", "middleright");
     document.getElementById("bc").textContent = '';
+    document.getElementById("bc").setAttribute("class", "bottomcenter");
     document.getElementById("bl").textContent = '';
+    document.getElementById("bl").setAttribute("class", "bottomleft");
     document.getElementById("br").textContent = '';
+    document.getElementById("br").setAttribute("class", "bottomright");
 }
 
 const checkStatus = function () {
@@ -57,52 +68,69 @@ const checkStatus = function () {
     t3 = document.getElementById("tr").textContent;
     t4 = document.getElementById("ml").textContent;
     t5 = document.getElementById("mc").textContent;
-    t9 = document.getElementById("mr").textContent;
-    t6 = document.getElementById("bl").textContent;
-    t7 = document.getElementById("bc").textContent;
-    t8 = document.getElementById("br").textContent;
+    t6 = document.getElementById("mr").textContent;
+    t7 = document.getElementById("bl").textContent;
+    t8 = document.getElementById("bc").textContent;
+    t9 = document.getElementById("br").textContent;
 
-    if (t1 === t2 && t2 === t3 && t1 != '') {
+    console.log('t1=' + t1, 't2=' + t2, 't3=' + t3, 't4=' + t4, 't5=' + t5, 't6=' + t6, 't7=' + t7, 't8=' + t8, 't9=' + t9)
+
+    if (t1 === t2 && t2 === t3 && t1 !== '') {
         winner = t1;
         gameOn = false;
+        highlightWinnerTiles(['tl', 'tc', 'tr'])
     };
 
-    if (t4 === t5 === t6 && t4 != ' ') {
+    if (t4 === t5 && t5 === t6 && t4 !== '') {
         winner = t4;
         gameOn = false;
+        highlightWinnerTiles(['ml', 'mc', 'mr'])
     };
 
-    if (t7 === t8 === t9 && t7 != ' ') {
+    if (t7 === t8 && t8 === t9 && t7 !== '') {
         winner = t7;
         gameOn = false;
+        highlightWinnerTiles(['bl', 'bc', 'br'])
     };
 
-    if (t1 === t4 === t7 && t1 != ' ') {
+    if (t1 === t4 && t4 === t7 && t1 !== '') {
         winner = t1;
         gameOn = false;
+        highlightWinnerTiles(['tl', 'bl', 'ml'])
     };
 
-    if (t2 === t5 === t8 && t2 != ' ') {
+    if (t2 === t5 && t5 === t8 && t2 !== '') {
         winner = t2;
         gameOn = false;
+        highlightWinnerTiles(['tc', 'bc', 'mc'])
     };
 
-    if (t3 === t6 === t9 && t3 != ' ') {
+    if (t3 === t6 && t6 === t9 && t3 !== '') {
         winner = t3;
         gameOn = false;
+        highlightWinnerTiles(['tr', 'br', 'mr'])
     };
 
-    if (t1 === t5 === t9 && t1 != ' ') {
+    if (t1 === t5 && t5 === t9 && t1 !== '') {
         winner = t1;
         gameOn = false;
+        highlightWinnerTiles(['tl', 'mc', 'br'])
     };
 
-    if (t3 === t5 === t7 && t3 != ' ') {
+    if (t3 === t5 && t5 === t7 && t3 !== '') {
         winner = t3;
         gameOn = false;
+        highlightWinnerTiles(['tr', 'mc', 'bl'])
     };
 
     return winner;
+}
 
+const highlightWinnerTiles = function (tiles) {
+    tiles.forEach((t) => {
+        var tile;
+        tile = document.getElementById(t);
+        tile.setAttribute("class", "winners")
+    })
 }
 
